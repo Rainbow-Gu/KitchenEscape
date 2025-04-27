516ea7f37ef60ba81c0f88f357412f8acc9a0811
MichelleXu00 <mxu32@smith.edu>
2025-04-26 21:51:04 -0400

    created containment instances in Game

--- Game.java
+++ Game.java
@@ -1,10 +1,14 @@
+import java.util.Scanner;
+
 public class Game {
     
     public static void main(String[] args) {
-        Containment Drawer1 = new Containment(Knife, Fork);
-        Containment Drawer2 = new Containment(Noodles, Flour);
-        Containment Drawer3 = new Containment(Candles, null);
-        Containment Fridge = new Containment(egg, null);
+        Containment drawer1 = new Containment(Knife, Fork);
+        Containment drawer2 = new Containment(Noodles, Flour);
+        Containment drawer3 = new Containment(Candles, null);
+        Containment fridge = new Containment(Egg, null);
+
+        Scanner input = new Scanner(System.in);
         
     }
 }
