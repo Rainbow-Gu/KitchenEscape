bb2d1a9fb1cb43298f3f54f3e7d380e131b4473e
Rainbow_Gu <rgu@smith.edu>
2025-05-05 20:56:55 -0400

    edit cabinet

--- Cabinet.java
+++ Cabinet.java
@@ -10,15 +10,33 @@ public class Cabinet extends Containment {
         items.add(item2);
     }
 
-    public boolean open () {
+    public boolean open() {
         if (collection.contains(key)) {
-            openClose = true;
-            System.out.println("You found a(n) " + items.get(0).getName()+ " and " + items.get(1).getName() + ".");
+            if (!openClose) {
+                openClose = true;
+                if (items.isEmpty()) {
+                    System.out.println("It is empty.");
+                } else {
+                    System.out.print("You found a(n) ");
+                    for (int i = 0; i < items.size(); i++) {
+                        Item item = items.get(i);
+                        if (i == 0) {
+                            System.out.print(item.getName());
+                        } else {
+                            System.out.print(" and " + item.getName());
+                        }
+                    }
+                    System.out.println(".");
+                }
+            } else {
+                System.out.println("It is already opened.");
+            }
             return openClose;
         } else {
             System.out.println("The door of the cabinet is locked.");
             return false;
         }
-        }
+    }
+    
 }
 
